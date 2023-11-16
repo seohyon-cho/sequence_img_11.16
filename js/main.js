a3768f1507f53b6b4005ab11658ad1cfbacaa6e2
seohyon-cho <flanique@naver.com>
2023-11-16 12:49:06 +0900

    동적으로 생성된 이미지 DOM을 인수로 받아서, 소스 이미지의 로딩 유무를 체크해주는 함수 생성

--- js/main.js
+++ js/main.js
@@ -5,23 +5,8 @@ const imgNum = 200;
 
 let count = 0;
 
-createImgs(frame, imgNum, 'pic');
-const imgs = frame.querySelectorAll('img');
-
-imgs.forEach((img) => {
-	img.addEventListener('error', (e) => {
-		e.currentTarget.setAttribute('src', 'img/logo.png');
-	});
-
-	img.addEventListener('load', () => {
-		console.log(count);
-		countEl.innerText = parseInt((count / imgNum) * 100) + 1;
-		count++;
-		if (count === imgNum) {
-			mask.remove();
-		}
-	});
-});
+const imgs = createImgs(frame, imgNum);
+imgLoadedCheck(imgs);
 
 frame.addEventListener('mousemove', (e) => {
 	const pageX = e.pageX;
@@ -32,11 +17,28 @@ frame.addEventListener('mousemove', (e) => {
 });
 
 // 동적으로 이미지 생성하는 함수
+// 돔 생성하자마자 바로 리턴해서 활용가능하도록 처리
 function createImgs(frame, imgNum, imgName = 'pic') {
 	let tags = '';
-
-	for (let i = 0; i < imgNum; i++) {
-		tags += `<img src='img/${imgName}${i}.jpg' />`;
-	}
+	for (let i = 0; i < imgNum; i++) tags += `<img src='img/${imgName}${i}.jpg' />`;
 	frame.innerHTML = tags;
+	return frame.querySelectorAll('img');
+}
+
+// 이미지 소스 로딩 유무 체크하는 함수
+function imgLoadedCheck(imgs) {
+	imgs.forEach((img) => {
+		img.addEventListener('error', (e) => {
+			e.currentTarget.setAttribute('src', 'img/logo.png');
+		});
+
+		img.addEventListener('load', () => {
+			console.log(count);
+			countEl.innerText = parseInt((count / imgNum) * 100) + 1;
+			count++;
+			if (count === imgNum) {
+				mask.remove();
+			}
+		});
+	});
 }

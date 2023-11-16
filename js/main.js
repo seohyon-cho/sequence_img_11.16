54a84dbbcc49c69fe33a58cc619f972a781058ed
seohyon-cho <flanique@naver.com>
2023-11-16 12:05:54 +0900

    이미지 소스 로딩 완료 시 마스크 제거

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@
 
 const frame = document.querySelector('figure');
 const imgNum = 200;
+const mask = document.querySelector('.mask');
+const countEl = mask.querySelector('span');
+
 let tags = '';
 
 // 1단계
@@ -20,7 +23,31 @@ for (let i = 0; i < imgNum; i++) {
 frame.innerHTML = tags;
 
 // 3, 4단계 (1)
+// 동적으로 200개의 DOM이 막 생성된 순간
 const imgs = frame.querySelectorAll('img');
+
+// 5단계
+let count = 0;
+// 각 동적 생성 이미지 요소를 반복 처리
+imgs.forEach((img) => {
+	img.addEventListener('error', (e) => {
+		// console.log('해당 이미지 로딩 실패', e);
+		// 만약 특정 이미지에서 에러 발생 시(엑박), 대체 이미지 출력
+		e.currentTarget.setAttribute('src', 'img/logo.png');
+	});
+	// 특정 이미지 렌더링 성공 시, count값 증가
+	img.addEventListener('load', () => {
+		console.log(count);
+		// 카운트값을 다시 백분율로 변환해서 로딩화면에 출력
+		countEl.innerText = parseInt((count / imgNum) * 100) + 1;
+		count++;
+		// 카운트갯수와 전체 이미지 갯수가 동일해지면 (모든 이미지 소스가 렌더링 완료 시)
+		// 마스크 제거
+		if (count === imgNum) {
+			mask.remove();
+		}
+	});
+});
 
 // 2단계
 frame.addEventListener('mousemove', (e) => {

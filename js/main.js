bf56e6966f49e7278621871afdbfc3dd66695b92
seohyon-cho <flanique@naver.com>
2023-11-16 13:01:08 +0900

    이벤트, 함수 분리

--- js/main.js
+++ js/main.js
@@ -3,18 +3,9 @@ const mask = document.querySelector('.mask');
 const countEl = mask.querySelector('span');
 const imgNum = 200;
 
-let count = 0;
-
 const imgs = createImgs(frame, imgNum);
 imgLoadedCheck(imgs);
-
-frame.addEventListener('mousemove', (e) => {
-	const pageX = e.pageX;
-	const percent = parseInt((pageX / window.innerWidth) * imgNum);
-
-	imgs.forEach((img) => (img.style.visibility = 'hidden'));
-	imgs[percent].style.visibility = 'visible';
-});
+frame.addEventListener('mousemove', showImg);
 
 // 동적으로 이미지 생성하는 함수
 // 돔 생성하자마자 바로 리턴해서 활용가능하도록 처리
@@ -27,6 +18,8 @@ function createImgs(frame, imgNum, imgName = 'pic') {
 
 // 이미지 소스 로딩 유무 체크하는 함수
 function imgLoadedCheck(imgs) {
+	let count = 0;
+
 	imgs.forEach((img) => {
 		img.addEventListener('error', (e) => {
 			e.currentTarget.setAttribute('src', 'img/logo.png');
@@ -41,4 +34,12 @@ function imgLoadedCheck(imgs) {
 			}
 		});
 	});
+}
+
+// 현재 마우스 포인터의 위치에 따른 이미지를 보이게 하는 함수
+function showImg(e) {
+	const pageX = e.pageX;
+	const percent = parseInt((pageX / window.innerWidth) * imgNum);
+	imgs.forEach((img) => (img.style.visibility = 'hidden'));
+	imgs[percent].style.visibility = 'visible';
 }
